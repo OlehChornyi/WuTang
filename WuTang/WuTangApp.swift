dee67ae9befb0ca34c870dc4ee08a5dc2cb3d09e
oleh <phd.chornyi@gmail.com>
2025-01-17 10:42:20 +0200

    Initial Commit

---
+++ WuTang/WuTangApp.swift
@@ -0,0 +1,17 @@
+//
+//  WuTangApp.swift
+//  WuTang
+//
+//  Created by Oleh on 17.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct WuTangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
