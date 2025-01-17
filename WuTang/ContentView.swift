070d9d54474769055548370caaac5e54b184d99f
oleh <phd.chornyi@gmail.com>
2025-01-17 11:16:57 +0200

    3rd: Basic UI

--- WuTang/ContentView.swift
+++ WuTang/ContentView.swift
@@ -8,14 +8,118 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var enteredName = ""
+    @State private var staticCoderNameIs = "Your Wu-Tang Coder Name Is:"
+    @State private var wuTangName = "Undefined Breakpoint"
+    
+    private let firstColumn = ["Algorithmic",
+                               "Byte",
+                               "Cache",
+                               "Debug",
+                               "Echo",
+                               "Function",
+                               "Git",
+                               "Hex",
+                               "Infinite",
+                               "Java",
+                               "Kernel",
+                               "Logic",
+                               "Module",
+                               "Node",
+                               "Object",
+                               "Pixel",
+                               "Query",
+                               "Runtime",
+                               "Script",
+                               "Token",
+                               "Undefined",
+                               "Virtual",
+                               "Web",
+                               "Xcode",
+                               "Yota",
+                               "Zero"]
+    private let secondColumn = ["$tack",
+                                "Processor",
+                                "Cipher",
+                                "Daemon",
+                                "EndPoint",
+                                "Framework",
+                                "Gateway",
+                                "Hub",
+                                "Interrupt",
+                                "Crash",
+                                "Loop",
+                                "Module",
+                                "Nexus",
+                                "Optimizer",
+                                "Protocol",
+                                "Queue",
+                                "Router",
+                                "Stack",
+                                "Thread",
+                                "Update",
+                                "Variable",
+                                "Widget",
+                                "Terminal",
+                                "Yield",
+                                "Zen",
+                                "Ace",
+                                "Breakpoint",
+                                "Root",
+                                "Instance",
+                                "Access",
+                                "Archive",
+                                "Control",
+                                "Justice"]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Wu-Tang\nCoder Name Generator")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundStyle(.yellow)
+                .multilineTextAlignment(.center)
+                .frame(maxWidth: .infinity)
+                .padding(.bottom)
+                .padding(.horizontal)
+                .background(.black)
+            
+            Spacer()
+            
+            TextField("Enter name here", text: $enteredName)
+                .font(.title2)
+                .textFieldStyle(.roundedBorder)
+                .overlay {
+                    RoundedRectangle(cornerRadius: 5)
+                        .stroke(.gray, lineWidth: 1)
+                }
+                .padding()
+            
+            Button(action: {}, label: {
+                Image("wu-tang-button")
+                Text("Get It!")
+            })
+            .font(.title2)
+            .fontWeight(.bold)
+            .foregroundStyle(.yellow)
+            .buttonStyle(.borderedProminent)
+            .tint(.black)
+            
+            
+            VStack{
+                Text(staticCoderNameIs)
+                Text(wuTangName)
+                    .fontWeight(.black)
+            }
+            .font(.largeTitle)
+            .frame(height: 130)
+            
+            Spacer()
+            
+            Image("wu-tang")
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
     }
 }
 
